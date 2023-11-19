4d2c66b457418cb6dc5195961e9f9135ac3bc753
ycarpiogonzalez@gmail.com <ycarpiogonzalez@gmail.com>
2023-11-19 08:13:39 -0300

    Un

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   root to: "pages#home"
 
   get "/up/", to: "up#index", as: :up
---
+++ db/migrate/20231119031313_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+      t.string :email
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
